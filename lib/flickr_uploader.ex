d2aa9d5edb672794c113c1dde35d0e82f5571927
Chuhei Ishigaki <chuhei1107@gmail.com>
2016-10-12 00:20:30 +0900

    refactoring

--- lib/flickr_uploader.ex
+++ lib/flickr_uploader.ex
@@ -6,23 +6,49 @@ defmodule FlickrUploader do
   @access_token_url "https://www.flickr.com/services/oauth/access_token"
   @upload_url "https:/www.flickr.com/services/upload/"
 
+  # Get Flickr API Consumer Key from Configuration
   defp api_key() do
     Application.get_env(:flickr_uploader, :api_key)
   end
 
+  # Get Flickr API Consumer Secret from Configuration
   defp secret() do
     Application.get_env(:flickr_uploader, :secret)
   end
 
-  def main(args \\ []) do
-    # 1. Getting a Request Token
-    creds = OAuther.credentials(consumer_key: api_key, consumer_secret: secret, method: :hmac_sha1)
+  # parse flickr api response body. for Getting Request Body, Exchange the Request Token. 
+  #  exchange from "param1=value1&param2=value2" to [param1: value1, param2: value2]
+  defp parse_response(body) do
+    body |> String.split("&") |> Enum.map(fn(x) -> String.split(x, "=") end) |> Enum.map(fn([x,y]) -> {String.to_atom(x), y} end)
+  end
+
+  # getting request token.
+  #  return : [oauth_callback_confirmed: <true/false>, oauth_token: <oauth_token>, oauth_token_secret: <oauth_token_secret>]
+  defp get_request_token(apikey, scrt) do
+    creds = OAuther.credentials(consumer_key: apikey, consumer_secret: scrt, method: :hmac_sha1)
     params = OAuther.sign("get", @request_token_url, [{"oauth_callback", @callback_url}], creds)
     {:ok, response} = HTTPoison.get(@request_token_url, [], [params: params])
-    request_token = response.body
-                 |> String.split("&")
-                 |> Enum.map(fn(x) -> String.split(x, "=") end)
-                 |> Enum.map(fn([x,y]) -> {String.to_atom(x), y} end)
+    parse_response(response.body)
+  end
+
+  # Exchange the Request Token for an Access Token.
+  #  return : [fullname: <fullname>, oauth_token: <oauth_token>, oauth_token_secret: <oauth_token_secret>, user_nsid: <user_nsid>, username: <username>]
+  defp exchange_to_access_token(apikey, scrt, oauth_token, oauth_token_secret, verifier) do
+    creds = OAuther.credentials(
+      consumer_key: apikey, 
+      consumer_secret: scrt, 
+      method: :hmac_sha1, 
+      token: oauth_token, 
+      token_secret: oauth_token_secret)
+    params = OAuther.sign("get", @access_token_url, [{"oauth_verifier", verifier}], creds)
+    {:ok, response} = HTTPoison.get(@access_token_url, [], [params: params])
+    parse_response(response.body)
+  end
+
+  @doc "This is main function for Flickr Uploader Application."
+  def main(args \\ []) do
+    # 1. Getting a Request Token
+    request_token = get_request_token(api_key, secret)
     oauth_token = request_token[:oauth_token]
     oauth_token_secret = request_token[:oauth_token_secret]
 
@@ -31,18 +57,12 @@ defmodule FlickrUploader do
     {_, location} = response.headers |> Enum.find(fn({x,_}) -> x == "location" end)
     IO.puts "Open your browser and access to below url. And input oauth_verifier."
     IO.puts location
-    oauth_verifier = IO.gets(:stdio, "OAuth verifier : ")
+    oauth_verifier = IO.gets(:stdio, "OAuth verifier : ") |> String.trim
 
     # 3. Exchange the Request Token for an Access Token
-    creds2 = OAuther.credentials(
-      consumer_key: api_key, 
-      consumer_secret: secret, 
-      method: :hmac_sha1, 
-      token: oauth_token, 
-      token_secret: oauth_token_secret)
-    params3 = OAuther.sign("get", @access_token_url, [{"oauth_verifier", String.trim(oauth_verifier)}], creds2)
-    IO.inspect params3
-    {:ok, response} = HTTPoison.get(@access_token_url, [], [params: params3])
-    IO.inspect response
+    access_token = exchange_to_access_token(api_key, secret, oauth_token, oauth_token_secret, oauth_verifier)
+    IO.inspect access_token
+
+    # TODO use Flickr API with access token.
   end
 end
